9d71c1bb762b1caea9828226097dfc553834dd90
tatiana <122471667+tatianar3@users.noreply.github.com>
2024-01-28 18:50:34 -0500

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -13,7 +13,9 @@ class Assignment2:
 # task 3
     def listAnniversaries(self):
         anniversaries = [year % 100 for year in range(self.year, self.currentYear, 10)
-                         if year <= self.currentYear and year % 100 != 0]
+                         if year <= self.currentYear]
+        if 0 in anniversaries:
+            anniversaries.remove(0)
         return anniversaries
 
 # task 4
